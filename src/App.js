cfb14137776abea41458ef6776153525c7c9ce91
Tiago <tiago.4lexandre@gmail.com>
2025-05-03 18:56:47 -0300

    Added GlobalStyle

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
-
-  li {
-    list-style: none;
-  }
 `;
 
 function App() {
